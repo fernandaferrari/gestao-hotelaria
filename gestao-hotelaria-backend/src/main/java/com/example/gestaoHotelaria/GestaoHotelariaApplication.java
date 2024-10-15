cdee961e24fd55afdc881808ef2dbec04719fea3
Fernanda Ferrari <dracarysnele@gmail.com>
2024-10-15 18:41:49 -0300

    Adicionando projeto backend

---
+++ gestao-hotelaria-backend/src/main/java/com/example/gestaoHotelaria/GestaoHotelariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestaoHotelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoHotelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoHotelariaApplication.class, args);
+	}
+
+}
